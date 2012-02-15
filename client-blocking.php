bfadd1176eff989028a4141bbff0ffa813799937
James Titcumb <james@asgrim.com>
2012-02-15 22:18:58 +0000

    Changed worker to do a YQL query to get some weather

--- client-blocking.php
+++ client-blocking.php
@@ -5,15 +5,43 @@
 $client = new GearmanClient();
 $client->addServer("127.0.0.1");
 
+// set the complete callback
 $client->setCompleteCallback("cbDone");
-$task1 = $client->addTask("add", json_encode(array(5,3,6)));
 
+// Construct a data object to request some weather for a location and units (c or f)
+$data = new stdClass();
+$data->location = "portsmouth, uk";
+$data->unit = "c";
+$task1 = $client->addTask("get_weather", json_encode($data));
+
+// Run the tasks (this blocks!)
 $result = $client->runTasks();
 
+// Callback function for when it's done
 function cbDone($task)
 {
-  echo "Task complete...\n";
-  var_dump($task->data());
-  echo "\n";
+	echo "Task [" . $task->functionName() . "] complete...\n";
+
+	switch ($task->functionName())
+	{
+		case "add":
+			echo "The sum is " . $task->data() . ".\n";
+			break;
+		case "get_weather":
+			$weather = json_decode($task->data());
+
+			echo "{$weather->title}\n";
+			echo str_repeat("-", strlen($weather->title)) . "\n\n";
+
+			echo "Current conditions:\n";
+			echo "{$weather->condition->text}, {$weather->condition->temp}deg\n\n";
+
+			echo "Forecast:\n";
+			foreach ($weather->forecast as $forecast)
+			{
+				echo "{$forecast->day} - {$forecast->text}. High: {$forecast->high} Low: {$forecast->low}\n";
+			}
+			break;
+	}
 }
 
--- worker.php
+++ worker.php
@@ -5,34 +5,68 @@
 $worker= new GearmanWorker();
 $worker->addServer('127.0.0.1');
 
-$worker->addFunction("add", "add_fn");
+$worker->addFunction("get_weather", "get_weather");
 
-while (1)
+console("Started worker script.");
+
+while ($worker->work())
 {
-  print "Waiting for job...\n";
+	console("--------------------------");
+	console("Waiting for new job");
 
-  $ret= $worker->work();
-
-  if ($worker->returnCode() != GEARMAN_SUCCESS)
-  {
-    break;
-  }
+	if ($worker->returnCode() != GEARMAN_SUCCESS)
+	{
+		console("It brokes: " . $worker->returnCode());
+		break;
+	}
 }
 
-function add_fn($job)
+/**
+ * Log something to the console
+ * @param string $text
+ */
+function console($text)
 {
-  echo "Handle job: " . $job->handle() . "\n";
+	echo date("Y-m-d H:i:s") . "   $text\n";
+}
 
-  $numbers = json_decode($job->workload());
+/**
+ * Get weather information
+ * @param GearmanJob $job
+ */
+function get_weather($job)
+{
+	console("New job: " . $job->handle() . " (" . __FUNCTION__ . ")");
 
-  echo "Adding.. 0";
-  $res = 0;
-  foreach($numbers as $num)
-  {
-    echo " + " . $num;
-    $res += $num;
-  }
+	$data = json_decode($job->workload());
 
-  echo " = " . $res . "\n";
-  return $res;
+	// Extract variables from provided data, with defaults
+	$location = $data->location ?: "portsmouth, uk";
+	$unit = $data->unit ?: "c";
+
+	// Build a YQL query
+	console("Building YQL query");
+	$yql_query = "use 'http://github.com/yql/yql-tables/raw/master/weather/weather.bylocation.xml' as we;";
+	$yql_query .= "select * from we where location=\"{$location}\" and unit='{$unit}'";
+
+	$yql_query_url = "https://query.yahooapis.com/v1/public/yql?q=" . urlencode($yql_query) . "&format=json";
+
+	// Make call with cURL
+	console("Executing YQL query");
+	$ch = curl_init($yql_query_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$json = curl_exec($ch);
+
+	// Tidy up the returned JSON
+	console("Extracting relevant data");
+	$obj = json_decode($json);
+	$new_json = json_encode($obj->query->results->weather->rss->channel->item);
+
+	// Sleep for fun...
+	console("Falling asleep here");
+	sleep(2);
+
+	console("Done, returning weather");
+
+	return $new_json;
 }
